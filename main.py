00ab97d53eb3636e3aaf255e1b06d39a156495b8
Steph St <steph.streete@outlook.com>
2020-09-28 22:23:25 +0000

    the statements i added to choose the jokes

---
+++ main.py
@@ -0,0 +1,14 @@
+print("Do you want to hear a joke?")
+print("Pick your favourite number!")
+
+number = int(input("Choose a number between 1 and 3!:"))
+
+if (number == 1):
+  print("Why did the chicken cross the road? To get to the other side!")
+elif (number == 2):
+  print("Why dont scientists trust atoms? Because they make up everthing!")
+elif (number == 3):
+  print("Why are pirates pirates? Because they arghhh!")
+
+
+
